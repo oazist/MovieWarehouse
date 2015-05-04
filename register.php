9f13809b58f3f95bdf58d9b35e53a5a782ac40e9
Administrator <Administrator@K6XOKINOXUVDSKA>
2015-05-04 16:43:54 -0700

    registration page

---
+++ register.php
@@ -0,0 +1,24 @@
+<?php
+
+	//include
+	require_once('includes/config.inc.php');
+
+    
+        $current = $_POST['userName'];
+        $current2 = $_POST['userId'];
+        $current3 = $_POST['userPass'];
+        $current4 = $_POST['userEmail'];
+        $current5 = $_POST['userCredit'];
+	
+	//connect Database
+	$link = mysqli_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD) or die("Could not connect to host");
+	mysqli_select_db($link, DB_DATABASE) or die("Could not find database");
+	
+	$sql = "INSERT INTO `user` (`name`, `username`, `password`, `email`, `creditcard`, `accountType`) "
+               ."VALUES ( '".$current."','".$current2."','".$current3."','".$current4."','".$current5."', '1');";
+        
+        $result = mysqli_query($link,$sql)or die("Data not found")
+                
+        //header("location:login.php");
+	
+?>
